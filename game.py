7b9851ed916f37a95547853ac147df13fae9f4fa
Jéssica Souza <jessica+pgp-signed-commit@souza.cloud>
2022-06-27 22:41:30 -0300

    add check_winner

--- game.py
+++ game.py
@@ -1,24 +1,62 @@
+import random
+
+
 class Game:
     def __init__(self):
         self.choices = ['r', 'p', 's']
 
-    #get user input
+    # get user input
     def input(self):
-        self.user_input = input(
+        self.user = input(
             "Press 'r' for rock, 'p' for paper or 's' for scissors: ")
-        print("user chose: ", self.user_input)  # test
-        
+        self.user = self.user.lower()
+        print("user chose: ", self.user)  # test
+
     # check user input
     def check_input(self):
         for i in range(len(self.choices)):
-            if self.user_input == self.choices[i]:
+            if self.user == self.choices[i]:
                 print("Correct input")  # meu
                 return i
         print("Incorrect input. Try again.")
         game.input()
-        game.check_input()
 
+    # get computer random input
+
+    def computer(self):
+        self.comp_input = random.choice(self.choices)
+
+        print(self.comp_input)
+
+        return self.comp_input
+
+    def check_winner(self):
+        if self.comp_input != self.user:
+            if self.comp_input == 'p':
+                if self.user == 'r':
+                    return 'l'
+                else:
+                    return 'w'
+
+            elif self.comp_input == 'r':
+                if self.user == 's':
+                    return 'l'
+                else:
+                    return 'w'
+
+            elif self.comp_input == 's':
+                if self.user == 'p':
+                    return 'l'
+                else:
+                    return 'w'
+
+        else:
+            return 't'
 
 game = Game()
+
 game.input()
 game.check_input()
+game.computer()
+check_winner = game.check_winner()
+game.print_winner()
