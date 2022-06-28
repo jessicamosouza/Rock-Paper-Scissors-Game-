69c146a36145ce605c9918093bd3abcee60333fc
Jéssica Souza <jessica+pgp-signed-commit@souza.cloud>
2022-06-27 21:07:28 -0300

    add check_input and improve code

--- game.py
+++ game.py
@@ -1,6 +1,24 @@
-from get_user_input import User
+class Game:
+    def __init__(self):
+        self.choices = ['r', 'p', 's']
+
+    #get user input
+    def input(self):
+        self.user_input = input(
+            "Press 'r' for rock, 'p' for paper or 's' for scissors: ")
+        print("user chose: ", self.user_input)  # test
+        
+    # check user input
+    def check_input(self):
+        for i in range(len(self.choices)):
+            if self.user_input == self.choices[i]:
+                print("Correct input")  # meu
+                return i
+        print("Incorrect input. Try again.")
+        game.input()
+        game.check_input()
 
 
-choices = ('r', 'p', 's')
-user = User()
-user.get_user_input(choices)
+game = Game()
+game.input()
+game.check_input()
--- get_user_input.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def get_user_input(self, choices):
-        user_input = input(
-            "Press 'r' for rock, 'p' for paper or 's' for scissors: ")
-        print("user chose: ", user_input)  # test
-
-        print(len(choices))
